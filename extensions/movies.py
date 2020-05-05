d1dab488e4da34c2cd2c4f54635e655008ca7296
MajorX500 <angola2001@gmail.com>
2020-05-05 21:16:24 +0100

    test 1.4

--- extensions/movies.py
+++ extensions/movies.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import json
 from fuzzywuzzy import fuzz, process
+from random import choice, shuffle
 
 class Movies(commands.Cog):
     """All movies saved"""
