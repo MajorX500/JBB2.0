86882796bb54c6296fcebdd54efce2f39c31acd1
MajorX500 <angola2001@gmail.com>
2020-05-05 23:31:39 +0100

    test 1.7

--- extensions/movies.py
+++ extensions/movies.py
@@ -8,12 +8,20 @@ class Movies(commands.Cog):
     """All movies saved"""
     def __init__(self, bot):
         self.bot = bot
-    
-    @commands.command(name='abc',
-                     description='watched movie list',
-                     brief='Watched list')
-    async def watched(self, ctx):
-        await ctx.send("defghijklmnopqrstuvwxyz")
 
-def setup(bot):
-    bot.add_cog(Movies(bot))
+        @commands.command(name='abc',
+                description='watched movie list',
+                brief='Watched list')
+    async def watched(self, ctx):
+        embed = discord.Embed(title = "Movies List",
+                description='Something very random xD',
+                color=0xEE8700)
+
+        with open(bot.MOVIES_PATH) as f:
+          data = json.load(f)
+
+        for key in data:
+            for value in data[key]:
+                embed.add_field(name = value, value = "dsa", inline=False);
+
+        await ctx.send(embed = embed)
