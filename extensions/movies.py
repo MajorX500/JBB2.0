addf6fd981f5ec4fc95feff91ec6f5cdd769b2b5
MajorX500 <angola2001@gmail.com>
2020-05-05 21:31:25 +0100

    test 1.5

--- extensions/movies.py
+++ extensions/movies.py
@@ -8,7 +8,6 @@ class Movies(commands.Cog):
     """All movies saved"""
     def __init__(self, bot):
         self.bot = bot
-        self.movies_dict = json.load(open(bot.MOVIES_PATH, 'r', encoding="utf8"))
     
     @commands.command(name='watched',
                      description='watched movie list',
