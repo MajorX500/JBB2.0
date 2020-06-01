cf717ae8ad9478295062b11afec12ef93cfaf026
Sofia Santos <44589228+RisingFisan@users.noreply.github.com>
2020-06-01 19:48:06 +0100

    Changed movies commands permissions
    Now they can be done by people with the "cinemabot" role, instead of being hard-coded to specific user IDs.
    It may not work, but I'm positive it does.

--- extensions/movies.py
+++ extensions/movies.py
@@ -34,58 +34,53 @@ class Movies(commands.Cog):
         await ctx.send(embed = embed)
     
     @commands.command(name='add_movie',description='',brief='')
+    @commands.has_role("cinemabot")
     async def addmovies(self, ctx, nome, genero, ano):
-        if ctx.message.author.id == 525678916019421197 or ctx.message.author.id == 472511559529398293:
+        with open('./db/movies.json') as f:
+            data = json.load(f)
+            temp = data['next']
 
-            with open('./db/movies.json') as f:
-                data = json.load(f)
-                temp = data['next']
+        # python object to be appended
+        y = {"nome":nome, 
+            "genero": genero, 
+            "ano": ano
+            } 
+        temp.append(y);
+        f.close();
 
-            # python object to be appended
-            y = {"nome":nome, 
-                "genero": genero, 
-                "ano": ano
-                } 
-            temp.append(y);
-            f.close();
-    
-            with open("./db/movies.json",'w') as f:
-                json.dump(data, f, indent=4)
+        with open("./db/movies.json",'w') as f:
+            json.dump(data, f, indent=4)
 
-            await ctx.send("Movie " +nome + " " + genero + " " + ano + " added to !watched")
-        else:
-            await ctx.send("haha admin go brrrr")
+        await ctx.send("Movie " + nome + " " + genero + " " + ano + " added to !watched")
 
     @commands.command(name='check', description='', brief='')
+    @commands.has_role("cinemabot")
     async def skipmovie(self, ctx):
-        if ctx.message.author.id == 525678916019421197 or ctx.message.author.id == 472511559529398293:
-            with open('./db/movies.json') as f:
-                data = json.load(f)
+        with open('./db/movies.json') as f:
+            data = json.load(f)
 
-            new_data = [];
-            i = 1;
-        
-            new_data_next = [];
-            for element in data["next"]:
-                if i>1:
-                    new_data.append(element);
-                else:
-                    result = element;
-                    i=i+1
+        new_data = [];
+        i = 1;
 
-            for element in data["finished"]:
-                new_data_next.append(element);
+        new_data_next = [];
+        for element in data["next"]:
+            if i>1:
+                new_data.append(element);
+            else:
+                result = element;
+                i=i+1
 
-            new_data_next.append(result);
-            data["next"] = new_data;
-            data["finished"] = new_data_next;
+        for element in data["finished"]:
+            new_data_next.append(element);
 
-            with open('./db/movies.json', 'w') as f:
-                data = json.dump(data, f)
+        new_data_next.append(result);
+        data["next"] = new_data;
+        data["finished"] = new_data_next;
 
-            await ctx.send('Watched movie')
-        else:
-            await cxt.send("haha admin go brrrrr")
+        with open('./db/movies.json', 'w') as f:
+            data = json.dump(data, f)
+
+        await ctx.send('Watched movie')
 
     @commands.command(name='user', description='', brief='')
     async def getuser(self, ctx):
@@ -93,7 +88,7 @@ class Movies(commands.Cog):
 
     @commands.command(name='!admin', description='', brief='')
     async def testpermission(self, ctx):
-        if ctx.message.author.id == 525678916019421197 or ctx.message.author.id == 472511559529398293 or ctx.message.author.id == 485178269193207848 or ctx.message.author.id == 423956774593363979:
+        if "cinemabot" in (role.name for role in ctx.message.author.roles):
             await ctx.send('YES :)')
         else:
             await ctx.send('NO :(')
